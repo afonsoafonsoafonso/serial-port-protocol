336e1937f073a16e0462332c40f9dc9855e572f8
Moises Rocha <mpdr200011@gmail.com>
2019-10-02 15:37:15 +0100

    receiver done

--- TP1/receiver/receiver.c
+++ TP1/receiver/receiver.c
@@ -1,23 +1,34 @@
+/*Non-Canonical Input Processing*/
+
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <termios.h>
 #include <fcntl.h>
-#include <unistd.h>
+#include <termios.h>
+#include <stdio.h>
 
 #define BAUDRATE B38400
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
 
-int main() {
-	int fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY);
-	
-	if (fd < 0) {
-		perror("/dev/ttyS0");
-		exit(1);
-	}
-	
-	struct termios oldtio, newtio;
-	if ( tcgetattr(fd,&oldtio) == -1) {
+volatile int STOP=FALSE;
+
+int main(int argc, char** argv)
+{
+    int fd;
+    struct termios oldtio,newtio;
+
+
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+  
+    
+    fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY );
+    if (fd <0) {perror("/dev/ttyS0"); exit(-1); }
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
       perror("tcgetattr");
       exit(-1);
     }
@@ -27,42 +38,57 @@ int main() {
     newtio.c_iflag = IGNPAR;
     newtio.c_oflag = 0;
 
+    /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 0;   
-    newtio.c_cc[VMIN]     = 1;
-    
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+
+
+
+  /* 
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    leitura do(s) pr�ximo(s) caracter(es)
+  */
+
+
+
     tcflush(fd, TCIOFLUSH);
-	
-	if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr - new");
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
       exit(-1);
     }
 
-	
-	char buffer[256];
-	char c;
-	int i = 0;
-	
-	while (1) {
-		read(fd, &c, 1);
-		buffer[i] = c;
-		i++;
-		if (c == '\0') {
-			break;
-		}
-	}
-	
-	printf("%s/n", buffer);
-	
-	write(fd, buffer, i);
-	
-	sleep(1);
-	
-	if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr - old");
-      exit(-1);
+    printf("New termios structure set\n");
+
+    char buf[255];
+    char c;
+    int i = 0;
+
+    while (STOP==FALSE) {
+      read(fd,&c,1);
+      buf[i] = c;
+      i++;
+      if (c == '\0') {
+        STOP = TRUE;
+      }
     }
-	
-	return 0;
+
+    printf("%s\n", buf);
+
+
+  /* 
+    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o 
+  */
+
+    tcflush(fd, TCIOFLUSH);
+
+    int res = write(fd, buf, i);
+    printf("%d bytes resent.\n", res);
+
+
+    tcsetattr(fd,TCSANOW,&oldtio);
+    close(fd);
+    return 0;
 }
