3f5cc4e67f16a20ee274f60572a33f5d804e7778
Moisés Rocha <moisesrocha99@gmail.com>
2019-10-10 09:43:46 +0100

    sender UA state machine

--- TP2/sender/sender.c
+++ TP2/sender/sender.c
@@ -1,13 +1,15 @@
 /*Non-Canonical Input Processing*/
 
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
-#include <termios.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
@@ -20,117 +22,169 @@
 #define C_SET 0x03
 #define C_UA 0x07
 
-#define MERDOU -1
+#define ERROR -1
 
-volatile int STOP=FALSE;
+volatile int STOP = FALSE;
 
-int main(int argc, char** argv)
-{
-    int fd, res;
-    struct termios oldtio,newtio;
-    char buf[255];
-    int sum = 0, speed = 0;
+int receiveUA(int serialPortFD) {
+  char c;
+  int nr;
+
+  enum state { START, FLAG_RCV, A_RCV, C_RCV, BCC_OK };
+  enum state curr = START;
+
+  char check = 0;
+
+  while (STOP == FALSE) {
+    nr = read(serialPortFD, &c, 1);
+    if (nr < 0) {
+      if (errno == EINTR) {
+        puts("Timed out. Sending again.");
+        return ERROR;
+      }
+      continue;
+    }
+    printf("nc = %d, %x\n", nr, (int)c);
+
+    switch (curr) {
+    case START:
+      if (c == FLAG) {
+        puts("Received Flag");
+        curr = FLAG_RCV;
+      }
+      break;
+    case FLAG_RCV:
+      if (c == A) {
+        puts("Received A");
+        curr = A_RCV;
+        check ^= c;
+      } else if (c == FLAG) {
+      } else {
+        curr = START;
+      }
+      break;
+    case A_RCV:
+      if (c == C_UA) {
+        puts("Received C_UA");
+        curr = C_RCV;
+        check ^= c;
+      } else if (c == FLAG) {
+        curr = FLAG_RCV;
+      } else {
+        curr = START;
+      }
+      break;
+    case C_RCV:
+      if (c == check) {
+        puts("BCC is ok");
+        curr = BCC_OK;
+      } else if (c == FLAG) {
+        curr = FLAG_RCV;
+      } else {
+        curr = START;
+      }
+      break;
+    case BCC_OK:
+      if (c == FLAG) {
+        puts("SET END");
+        STOP = TRUE;
+      } else {
+        curr = START;
+      }
+      break;
+    }
+  }
+  puts("Exiting.");
+}
+
+int main(int argc, char **argv) {
+  int fd, res;
+  struct termios oldtio, newtio;
+  char buf[255];
+  int sum = 0, speed = 0;
 
   /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
   */
 
+  fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY);
+  if (fd < 0) {
+    perror(argv[1]);
+    exit(-1);
+  }
 
-    fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
+  if (tcgetattr(fd, &oldtio) == -1) { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
+  memset(&newtio, 0, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
 
-    memset(&newtio, 0, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
 
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
-
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
-
-
+  newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
+  newtio.c_cc[VMIN] = 1;  /* blocking read until 5 chars received */
 
   /*
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
     leitura do(s) pr�ximo(s) caracter(es)
   */
 
+  tcflush(fd, TCIOFLUSH);
 
+  if (tcsetattr(fd, TCSANOW, &newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
-    tcflush(fd, TCIOFLUSH);
+  printf("New termios structure set\n");
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
+  // set masking
+  // UA
 
-    printf("New termios structure set\n");
-
-    //set masking
+  while (STOP == FALSE) {
     unsigned char set[5];
-    set[0]=FLAG;
-    set[1]=A;
-    set[2]=C_SET;
-    set[3]=set[1]^set[2];
-    set[4]=FLAG;
-    //set sending
+    set[0] = FLAG;
+    set[1] = A;
+    set[2] = C_SET;
+    set[3] = set[1] ^ set[2];
+    set[4] = FLAG;
+    // set sending
     write(fd, set, 5);
-    //UA
-    unsigned char ua[5];
-    //for(int i=0; i<5; i++) {
-        printf("waiting for ua1\n");
-        read(fd, &ua, 5);
-        printf("waiting for ua2\n");
-    //}
-    //UA check
-    if(ua[0]!=FLAG || ua[1]!=A || ua[2]!=C_UA || ua[3]!=(A^C_UA) || ua[4]!=FLAG) {
-      printf("FLAG:%x \n A:%x \n UA:%x \n BCC:%x \n FLAG:%x \n", ua[0], ua[1], ua[2], ua[3], ua[4]);
-      printf("ESTA UA BYTE ESTÁ OH, ASSIM, UMA BALENTE MERDA\n");
-      return MERDOU;
+    alarm(5);
+
+    if (receiveUA(fd) == 0) {
+      STOP = TRUE;
     }
+  }
 
-    for (int i = 0; i < 25; i++) {
-      buf[i] = 'a';
-    }
+  for (int i = 0; i < 25; i++) {
+    buf[i] = 'a';
+  }
 
-    /*testing*/
-    buf[25] = '\0';
+  /*testing*/
+  buf[25] = '\0';
 
-    res = write(fd,buf,26);
-    printf("%d bytes written\n", res);
+  res = write(fd, buf, 26);
+  printf("%d bytes written\n", res);
 
   /*
-    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar
-    o indicado no gui�o
+    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a
+    respeitar o indicado no gui�o
   */
 
-    int i = 0;
-    char c;
+  printf("%s\n", buf);
 
-    while(1) {
-      read(fd, &c, 1);
-      buf[i] = c;
-      i++;
-      if (c == '\0') {
-        break;
-      }
-    }
+  if (tcsetattr(fd, TCSANOW, &oldtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
-    printf("%s\n", buf);
-
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
-
-    close(fd);
-    return 0;
+  close(fd);
+  return 0;
 }
