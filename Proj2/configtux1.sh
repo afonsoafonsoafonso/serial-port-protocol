cddcd18ad946c378c11990c7323347848ccbae14
Moisés Rocha <moisesrocha99@gmail.com>
2019-11-27 23:34:29 +0000

    tux config files

---
+++ Proj2/configtux1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+/etc/init.d/networking restart
+
+ifconfig eth0 down
+ifconfig eth1 down
+ifconfig eth0 up
+ifconfig eth0 172.16.50.1/24
+
+route add -net 172.16.51.0/24 gw 172.16.50.254
+route add default gw 172.16.50.254
+
+echo "search netlab.fe.up.pt\nnameserver 172.16.1.1" > /etc/resolv.conf
